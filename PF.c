3aa8ce57e820c6256bdf981e559c122615d4f8ea
Victoria Guarnieri <84190435+vickyguar@users.noreply.github.com>
2022-10-23 13:29:25 -0300

    Actualización PF
    Agrego versiones actualizadas y comentadas de PF.c y PF.h

--- PF.c
+++ PF.c
@@ -1,117 +1,89 @@
 #include <PF.h>
-#define use port_b_lcd TRUE
-#include <lcd.c>
 
 #BYTE TRISA = 0x85
 #BYTE TRISB = 0x86
 #BYTE PORTA = 0x05
+#BYTE PORTB = 0x06
 
-#define N_tmr0 60 //Se calculó el N_tmr0 para 3 segundos
-#define N 12
-
-// Vector de frecuancias
-int FrecuenciasARegistrar[N] = { 125, 250, 500, 750, 1000, 1500, 2000, 3000, 4000, 6000, 8000 };
-
-// Variables
 int Frecuencia = FrecuenciasARegistrar[0]; // Frecuencia inicializada en 125
-int N_tmr1 = 0;
-int contador_tmr0 = 0;
-int nueva_frec = 0;
-int terminarMenu = 0;
 
-//Funciones
-void Menu();
-void Seteo_Senal();  // Seteo de la senal según la frecuencia
-void Enviar_Senal(); // Inicialización de timer0 y timer1
-void Finalizar();    // Datos a la eeprom
-void Comunicar();
-
-// EStructura
-struct sDatos{      
-   int listening; //1 si está escuchando    
-   int freq;      
-   int dB;      
-   int ear; //1 derecho
-};
+// Variables globales
+int N_tmr1 = 0;         // Valor que toma el tmr1
+int contador_tmr0 = 0;  // Para realizar ciclos de repetición en el tmr1
+int pos_frec = 0;       // Indica la posicion "actual" en la lista sDatos y FrecuenciasARegistrar
+int nueva_frec = 0;     // Booleano para ver si termino la frecuencia de registro
 
 
-struct sDatos lista[N];
+struct sDatos lista[N]; // Lista de sDatos
 
-
-// Interrupción de timer1 la utilizamos para generar la señal cuadrada en el pin B7
+//Esta interrupcion la utilizamos para generar la senal cuadrada en el pin A7
 #int_TIMER1
 void TIMER1_isr() {
-   output_toggle(PIN_B7);
+   output_toggle(PIN_A7);
    set_timer1(N_tmr1);
 }
 
-// Interrupción de timer0 la utilizamos para registrar el NO PULSADO/ NO ESCUCHO
+//Esta interrupcion la utilizamos para registrar el NO PULSADO
 #int_TIMER0
 void TIMER0_isr() {
-   contador_tmr0 += 1;
+   contador_tmr0++;
    if (contador_tmr0 == 6) {
-      //registrar no escucho para ESCRIBIR EN EEPROM 
+      lista[pos_frec].listening = 0; // No escuchó
       contador_tmr0 = 0;
       nueva_frec = 1;
    }
+
 }
 
-// Interrupción externa RB0 la utilizamos para registrar el PULSADO/ ESCUCHO al presionar el boton en RB0
+//Esta interrupcion la utilizamos para registrar el PULSADO al presionar el boton de escucha en RB0
 #int_ext
 void ext_isr() {
-   //registrar escucho para ESCRIBIR EN EEPROM
+   lista[pos_frec].listening = 1; // Sí escuchó
    disable_interrupts(int_TIMER0);
    nueva_frec = 1;
 }
 
-// aca hay que poner una variable para que se interrumpa con el boton de rb4 a rb7
-#int_RB
-void RB_isr() {
-
-   terminarMenu = 1;
-
-}
 
 void main() {
-   
    // Seteo de los timers con Prescaler = 256
    setup_timer_0(RTCC_INTERNAL | RTCC_DIV_256);
    setup_timer_1(RTCC_INTERNAL | RTCC_DIV_256);
 
-   // Seteo de entradas analógicas
-   setup_adc_ports(ALL_ANALOG);
-   setup_adc(ADC_CLOCK_INTERNAL);
+   // Seteos de entrada/ salida
+   TRISB = 0x04;
+   TRISA = 0x40;
 
    // Usamos LCD
    lcd_init();
- 
+   
    // Mostramos el menu
    Menu();
    
-   int i = 0; // Iterador
-   
+   printf(lcd_putc, "/f");
+   printf(lcd_putc, "Toma de datos en curso");
+
    do {
-      Frecuencia = FrecuenciasARegistrar[i];
-      
+      Frecuencia = FrecuenciasARegistrar[pos_frec];
+      lista[pos_frec].freq = Frecuencia;
+
       // Habilitamos interrupciones
       enable_interrupts(int_ext);      // Interrupción externa RB0
-      Ext_int_edge(H_to_L);            
+      Ext_int_edge(H_to_L);
       enable_interrupts(int_TIMER0);   // Interrupción de timer0
       enable_interrupts(int_TIMER1);   // Interrupcion de timer1
       enable_interrupts(GLOBAL);       // Llave maestra
 
       // Seteo de timer1 según la frecuencia de análisis
       Seteo_Senal();
-      
+
       delay_ms(1000);
 
-      while (!nueva_frec)
-         Enviar_Senal();
-         
-      i++;
+      while (nueva_frec == 0) //!nueva_frec
+         Enviar_Senal();  // se envia durante 3 segundos la misma frecuencia hasta realizar la escritura en la eeprom
+
+      pos_frec++;
 
    } while (Frecuencia != 8000);
-
 
    Finalizar();
 
@@ -119,36 +91,45 @@ void main() {
 
 }
 
-// Pulsador de Menu RB3
+
 void Menu() {
-   Lcd_init(); // ¡CUIDADO!
+
+   // Variables
    signed char x1 = 16;
    signed char x0 = 16;
    signed char x3 = 16;
 
-   while (!terminarMenu) {
+   while (input(PIN_B3)) {
+      
       if (x0 >= -24) {
          Lcd_gotoxy(x0, 1);
          printf(lcd_putc, "Frecuencias a registrar: ");
+         
          delay_ms(100);
-         x0 = x0 - 1;
+         
+         x0 = x0 - 1; //x0--;
       }
-
-
+      
       if (x0 < -24 && x1 >= -47) {
          printf(lcd_putc, "\f");
+         
          Lcd_gotoxy(x1, 0);
          printf(lcd_putc, "125, 250, 500, 750, 1K, 1K5, 2K, 3K, 4K, 6K, 8K \n");
+         
          delay_ms(100);
-         x1 = x1 - 1;
+         
+         x1 = x1 - 1; //x1--;
       }
 
       if (x1 < -47) {
          printf(lcd_putc, "\f");
+         
          Lcd_gotoxy(x3, 1);
          printf(lcd_putc, "Pulse el boton para comenzar");
+         
          delay_ms(100);
-         x3 = x3 - 1;
+         
+         x3 = x3 - 1; //x3--;
       }
       
       if (x3 < -28) {
@@ -158,8 +139,8 @@ void Menu() {
       }
 
    }
-   
-   return;
+
+
 }
 
 void Seteo_Senal() {
@@ -167,23 +148,24 @@ void Seteo_Senal() {
    float T0 = 1 / Frecuencia; // Periodo
    float T = T0 / 2; // Subperiodo
    N_tmr1 = 256 - (T * 4000000 / (4 * 256));
+
 }
 
-// Se envia durante 3 segs la frecuencia hasta realizar la escritura en la EEPROM
 void Enviar_Senal() {
-   //inicializacion de timers
-   set_timer0(N_tmr0); 
+   // Inicialización de timers
+   set_timer0(N_tmr0);
    set_timer1(N_tmr1);
    
-   output_high(PIN_A7); //pin de salida de la patita
+   output_high(PIN_B7); //pin de salida de la patita
 }
 
 void Finalizar() {
-   printf(lcd_putc, "Se registraron todas las mediciones");
-   delay_ms(1500);
-   printf(lcd_putc, "\f");
-   // Datos a la EEPROM
+   printf(lcd_putc, "Se registraron todas las mediciones /f");
+   // DATOS A LA EEPROM
 }
 
 void Comunicar() {}
 
+// Esta vuelta es para un oido
+// Antes del Comunicar() repetir para 2do oido
+// Apretar el boton de Menu
---
+++ PF.h
@@ -0,0 +1,24 @@
+#include <16F1827.h>
+#device ADC = 10
+#use delay(clock = 4000000, crystal = 4MHz)
+#define use port_b_lcd TRUE
+#include <lcd.c>
+
+#define N 12
+#define N_tmr0 60 //Se calculó el N_tmr0 para 3 segundos
+
+typedef struct sDatos{ 
+int listening;
+int freq;      
+int dB; // Stand by      
+int ear;
+} sData;
+
+int FrecuenciasARegistrar[N] = {125, 250, 500, 750, 1000, 1500, 2000, 3000, 4000, 6000, 8000};
+
+void Menu();
+void Seteo_Senal();  // Seteo de la senal segun la frecuencia
+void Enviar_Senal(); // Inicializacion de timers
+void Finalizar();    // Datos a la EEPROM
+void Comunicar();
+
