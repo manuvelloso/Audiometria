62d8777baa8d60d1ebd9fafd738d35c670ba2c9c
Manuela Velloso <65735915+manuvelloso@users.noreply.github.com>
2022-10-23 13:27:51 -0300

    Delete bocetoPF.h

--- bocetoPF.h
+++
@@ -1,14 +0,0 @@
-#include <16F1827.h>
-#device ADC=10
-#use delay(crystal=4)
-
-typedef struct data
-{ int listening;   int freq;      int dB;      int ear;}sData;
-
-int FrecuenciasARegistrar[12]={125,250,500,750,1000,1500,2000,3000,4000,6000,8000};
-
-void Menu();
-void Seteo_Senal();  // seteo de la senal segun la frecuencia
-void Enviar_Senal(); //inicializacion de timers
-void Finalizar(); //Datos a la eeprom
-void Comunicar();
