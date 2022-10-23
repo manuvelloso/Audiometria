8c283533eabf5ee01ae462516098b2ef94bcbcec
Manuela Velloso <65735915+manuvelloso@users.noreply.github.com>
2022-10-23 13:46:11 -0300

    Create funciones.h

---
+++ PROYECTO AUDIOMETRIA/funciones.h
@@ -0,0 +1,8 @@
+#include <interrupciones.c>
+
+//Funciones
+void Menu();
+void Seteo_Senal();  // Seteo de la senal según la frecuencia
+void Enviar_Senal(); // Inicialización de timer0 y timer1
+void Finalizar();    // Datos a la eeprom
+void Comunicar();
