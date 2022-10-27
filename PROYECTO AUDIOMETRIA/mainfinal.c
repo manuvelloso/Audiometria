ef86facd1831de14dda730aeaa9cd13543a58b52
Victoria Guarnieri <84190435+vickyguar@users.noreply.github.com>
2022-10-27 14:14:42 -0300

    Update mainfinal.c

--- PROYECTO AUDIOMETRIA/mainfinal.c
+++ PROYECTO AUDIOMETRIA/mainfinal.c
@@ -3,8 +3,8 @@
 void main() {
    
    // Seteo de los timers con Prescaler = 256
-   setup_timer_0(RTCC_INTERNAL | RTCC_DIV_256);
    setup_timer_1(RTCC_INTERNAL | RTCC_DIV_256);
+   setup_timer_0(RTCC_INTERNAL | RTCC_DIV_256);
 
    // Seteo de entradas analógicas
    setup_adc_ports(ALL_ANALOG);
@@ -24,8 +24,8 @@ void main() {
       // Habilitamos interrupciones
       enable_interrupts(int_ext);      // Interrupción externa RB0
       Ext_int_edge(H_to_L);            
-      enable_interrupts(int_TIMER0);   // Interrupción de timer0
-      enable_interrupts(int_TIMER1);   // Interrupcion de timer1
+      enable_interrupts(int_TIMER1);   // Interrupción de timer0
+      enable_interrupts(int_TIMER0);   // Interrupcion de timer1
       enable_interrupts(GLOBAL);       // Llave maestra
 
       // Seteo de timer1 según la frecuencia de análisis
